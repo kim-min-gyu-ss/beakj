dbf4ae4f78a61b97d57c01a6775ad64342332a93
kim-min-gyu-ss <121535779+kim-min-gyu-ss@users.noreply.github.com>
2023-01-31 22:41:09 +0900

    [Bronze I] Title: 색종이, Time: 1100 ms, Memory: 39164 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,14 @@
+result = [[0 for i in range(1001)] for j in range(1001)]
+a = int(input())
+for i in range(1, a + 1):
+    x_1, y_1, x_2, y_2 = map(int,input().split())
+    for j in range(x_1, x_1 + x_2):
+        for k in range(y_1, y_1 + y_2):
+            result[k][j] = i
+for l in range(1,a + 1):
+    count = 0
+    for m in range(1001):
+        for n in range(1001):
+            if result[m][n] == l:
+                count += 1
+    print(count)
