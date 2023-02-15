cf4435043887032b87c6092f75c1b496ec1858a0
kim-min-gyu-ss <121535779+kim-min-gyu-ss@users.noreply.github.com>
2023-02-15 22:41:12 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,22 @@
+def back():
+    if len(stack) == M:
+        # for i in range(len(stack) - 1):
+        #     if stack[i] > stack[i+1]:
+        #         return
+        print(*stack, sep = ' ')
+        return
+    for i in num_list:
+        if visited[i]:
+            continue
+        visited[i] = True
+        stack.append(i)
+        back()
+        stack.pop()
+        visited[i] = False
+
+
+N, M = map(int, input().split())
+num_list = sorted(list(map(int,input().split())))
+visited = [False] * (num_list[-1]+1)
+stack = []
+back()
