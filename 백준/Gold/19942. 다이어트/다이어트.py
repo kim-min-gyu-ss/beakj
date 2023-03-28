89366d1ff59a669a1e01ea70209bf337c119f72e
kim-min-gyu-ss <121535779+kim-min-gyu-ss@users.noreply.github.com>
2023-03-28 16:54:35 +0900

    [Gold V] Title: 다이어트, Time: 168 ms, Memory: 116544 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.py
+++ 백준/Gold/19942. 다이어트/다이어트.py
@@ -1,55 +1,42 @@
+def back(n,lst,c_lst):
+    global mn, y_lst
+    if c_lst[4] >= mn:
+        return
+    if n == N:
+        return
+    if c_lst[0] >= a and c_lst[1] >= b and c_lst[2] >= c and c_lst[3] >= d:
+        if mn > c_lst[4]:
+            mn = c_lst[4]
+            y_lst = lst[:]
+        return
+    for i in range(n,N):
+        if not visited[i]:
+            visited[i] = True
+            c_lst[0] += yachae[i][0]
+            c_lst[1] += yachae[i][1]
+            c_lst[2] += yachae[i][2]
+            c_lst[3] += yachae[i][3]
+            c_lst[4] += yachae[i][4]
+            lst.append(i+1)
+            back(i,lst, c_lst)
+            c_lst[0] -= yachae[i][0]
+            c_lst[1] -= yachae[i][1]
+            c_lst[2] -= yachae[i][2]
+            c_lst[3] -= yachae[i][3]
+            c_lst[4] -= yachae[i][4]
+            lst.pop()
+            visited[i] = False
 N = int(input())
-def bt(now, end, is_used):
-    global ans_used, ans_money
+a,b,c,d = map(int,input().split())
+yachae = [list(map(int,input().split()))for _ in range(N)]
+mn = 500*N
+visited = [False for _ in range(N)]
+y_lst = []
+c_lst = [0,0,0,0,0]
+back(0,[], c_lst)
 
-    if energy[4] >= ans_money: # 현재 비용과 ans_money 비교
-        # 비교되는 시점부터 이후기 때문에, 고려 x
-        return
-
-    if (energy[0] >= have[0]) and (energy[1] >= have[1]) and (energy[2] >= have[2]) and (energy[3] >= have[3]):
-        if energy[4] < ans_money:
-            ans_money = energy[4]
-            ans_used = is_used[:]
-        return
-
-    if now == end:
-        return
-
-    for i in range(now,N+1):
-        if is_travel[i]: continue
-
-        is_travel[i] = True
-        is_used.append(i)
-        energy[0] += foods[i][0]
-        energy[1] += foods[i][1]
-        energy[2] += foods[i][2]
-        energy[3] += foods[i][3]
-        energy[4] += foods[i][4]
-
-        bt(i, end, is_used)
-    
-        is_travel[i] = False
-        is_used.pop()
-        energy[0] -= foods[i][0]
-        energy[1] -= foods[i][1]
-        energy[2] -= foods[i][2]
-        energy[3] -= foods[i][3]
-        energy[4] -= foods[i][4]
-
-
-# 단백질, 지방, 탄수화물, 비타민
-have = list(map(int,input().split()))
-
-foods = [0]+[list(map(int,input().split())) for _ in range(N)]
-is_travel = [False]*(N+1)
-
-ans_money = 500*N
-ans_used = []
-
-energy = [0, 0, 0, 0, 0]
-bt(1,N+1,[])
-if ans_used:
-    print(ans_money)
-    print(*ans_used) 
+if y_lst:
+    print(mn)
+    print(*y_lst)
 else:
     print(-1)
