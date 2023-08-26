e46060f9a3f23207a7f42659314156f8a42e75c6
kim-min-gyu-ss <121535779+kim-min-gyu-ss@users.noreply.github.com>
2023-08-26 23:53:48 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,17 @@
+	import java.util.*;
+ 
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {            
+            map.put(name[i], yearning[i]);
+        }
+        for (int i = 0; i < photo.length; i++) {
+            for (String item: photo[i]) {
+                answer[i] += map.getOrDefault(item, 0);
+            }
+        }
+        return answer;
+    }
+}
