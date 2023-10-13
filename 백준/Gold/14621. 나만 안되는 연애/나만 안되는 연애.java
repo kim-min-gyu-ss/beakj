e59537738d9235f97486d3be2fda080b261eaf25
kim-min-gyu-ss <121535779+kim-min-gyu-ss@users.noreply.github.com>
2023-10-13 10:21:14 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 300 ms, Memory: 24212 KB -BaekjoonHub

---
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M, start, end, wei;
+    static List<Node> info = new ArrayList<>();
+    static int[] parents;
+    static String[] parentsStr;
+    static class Node {
+        int start;
+        int end;
+        int wei;
+        public Node(int start, int end, int wei) {
+            this.start = start;
+            this.end = end;
+            this.wei = wei;
+        }
+        public int getWei() {
+            return this.wei;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parents = new int[N + 1];
+        parentsStr = new String[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N + 1; i++) {
+            parents[i] = i;
+            if (i == 0) {
+                parentsStr[i] = "N";
+            } else {
+                parentsStr[i] = st.nextToken();
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
+            wei = Integer.parseInt(st.nextToken());
+            info.add(new Node(start,end,wei));
+        }
+        Collections.sort(info,Comparator.comparing(Node::getWei));
+        System.out.println(tree(N));
+    }
+
+    private static int find(int x) {
+        if (parents[x] == x) return x;
+        parents[x] = find(parents[x]);
+        return parents[x];
+    }
+    private static int tree(int N) {
+        int result = 0;
+        int cnt = 0;
+        for (Node node : info) {
+            int x = find(node.start);
+            int y = find(node.end);
+            if (x != y && !parentsStr[node.start].equals(parentsStr[node.end])) {
+                parents[y] = x;
+                result += node.wei;
+                cnt++;
+            }
+        }
+        return cnt == N - 1 ? result : -1;
+    }
+}
